b98082615061cc79020c3377711d3148d0af4cd7
Mohamed Khalifa <mohamedkhalifa@Mohameds-MacBook-Air.local>
2019-05-14 19:56:38 -0500

    finally

--- rest-individuals/FantasyRestApi/src/main/java/com/revature/rest/FantasyResourse.java
+++
@@ -1,9 +0,0 @@
-package com.revature.rest;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class FantasyResourse extends ResourceConfig {
-	public FantasyResourse() {
-		packages("com.revature.rest");
-	}
-}
