731f1938fd8d1afa6f28ced39dcb6acc26ae78f5
Mohamed Khalifa <mohamedkhalifa@Mohameds-MacBook-Air.local>
2019-05-17 18:02:16 -0500

    first

---
+++ rest-individuals/RestApiFantasy/src/main/java/com/revature/rest/FantasyPoints.java
@@ -0,0 +1,50 @@
+package com.revature.rest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+@Path("/scores")
+public class FantasyPoints {
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response scores(){
+		List<BigDecimal> list=new ArrayList<>();
+		SimWeek simweek=new SimWeek();
+		HashMap<String,Double> hashmap=new HashMap();
+		List<Player> players=simweek.getAllPlayers();
+		for(Player p:players){
+			hashmap.put(p.getF_team(), 0.0);
+			
+		}
+		for(Player p:players){
+			double temp=hashmap.get(p.getF_team());
+			temp+=p.getFps();
+			hashmap.put(p.getF_team(),temp);
+			
+		}
+		for(Player p:players){
+			
+			BigDecimal temp=new BigDecimal(hashmap.get(p.getF_team()));
+			if(!list.contains(temp)){
+			list.add(temp);
+			}
+			
+		}
+		Gson gson=new Gson();
+		String jsonString = gson.toJson(list);
+		return Response.status(Response.Status.OK).entity(jsonString).build();
+		
+	}
+}
